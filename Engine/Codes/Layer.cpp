c696affa4a24fbc646b3f3b7d21078b8356860ca
DongHwan2780 <87415362+DongHwan2780@users.noreply.github.com>
2021-11-15 22:08:50 +0900

    asd
    asd

--- Engine/Codes/Layer.cpp
+++ Engine/Codes/Layer.cpp
@@ -1,12 +1,62 @@
 #include "..\Headers\Layer.h"
-
+#include "Obj.h"
 
 
 CLayer::CLayer()
 {
 }
 
-
-CLayer::~CLayer()
+HRESULT CLayer::Add_GameObjectToLayer(CObj * pGameObj)
 {
+	if (pGameObj == nullptr)
+		return E_FAIL;
+
+	m_ObjList.push_back(pGameObj);
+
+	return S_OK;
+}
+
+_int CLayer::Update(_double DeltaTime)
+{
+	_int	iProgress = 0;
+
+	for (auto& pGameObj : m_ObjList)
+	{
+		iProgress = pGameObj->Update(DeltaTime);
+
+		if (0 > iProgress)
+			return -1;
+	}
+
+	return _int();
+}
+
+_int CLayer::Late_Update(_double DeltaTime)
+{
+	_int	iProgress = 0;
+
+	for (auto& pGameObj : m_ObjList)
+	{
+		iProgress = pGameObj->Late_Update(DeltaTime);
+
+		if (0 > iProgress)
+			return -1;
+	}
+
+	return _int();
+}
+
+CLayer * CLayer::Create()
+{
+	CLayer*	pInstance = new CLayer();
+
+	return pInstance;
+}
+
+void CLayer::Free()
+{
+	for (auto& pGameObj : m_ObjList)
+		Safe_Release(pGameObj);
+
+	m_ObjList.clear();
 }
