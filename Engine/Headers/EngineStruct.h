f6b2ad1b0e4d6e4ecb30c6b3ee17722952c78634
DongHwan2780 <87415362+DongHwan2780@users.noreply.github.com>
2021-11-18 22:06:18 +0900

    asd
    asd

---
+++ Engine/Headers/EngineStruct.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef __ENGINESTRUCT_H__
+#define __ENGINESTRUCT_H__
+
+namespace Engine
+{
+	typedef struct tagVertexInfo
+	{
+		XMFLOAT3	vPos;
+		XMFLOAT2	vTexUV;
+	};
+
+	typedef struct tagPolygonIndices16
+	{
+		_ushort	_0, _1, _2;
+	}POLYGONINDICES16;
+
+	typedef struct tagPolygonIndices32
+	{
+		_ulong	_0, _1, _2;
+	}POLYGONINDICES32;
+}
+
+
+#endif // ! __ENGINESTRUCT__H_-
