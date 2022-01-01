128fa78da2cc439128fbf89377f02724d8b68259
DongHwan2780 <87415362+DongHwan2780@users.noreply.github.com>
2022-01-01 21:12:16 +0900

    asd
    asd

---
+++ KatamariDamacy/Client/Codes/Can.cpp
@@ -0,0 +1,151 @@
+#include "stdafx.h"
+#include "..\Headers\Can.h"
+
+#include "Management.h"
+
+CCan::CCan(DEVICES)
+	:CObjList(pDevice, pDeviceContext)
+{
+}
+
+CCan::CCan(const CCan & other)
+	: CObjList(other)
+{
+}
+
+HRESULT CCan::Initialize_Prototype()
+{
+	if (FAILED(__super::Initialize_Prototype()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CCan::Initialize_Clone(void * pArg)
+{
+	if (FAILED(__super::Initialize_Clone(pArg)))
+		return E_FAIL;
+
+	if (FAILED(SetUp_Components()))
+		return E_FAIL;
+
+	_float3 vPickingPos = _float3{0.f ,0.f, 0.f};
+	memcpy(&vPickingPos, pArg, sizeof(_float3));
+
+	m_pTransform->Set_State(CTransform::POSITION, XMLoadFloat3(&vPickingPos));
+
+	return S_OK;
+}
+
+_int CCan::Update(_double DeltaTime)
+{
+	if (0 > __super::Update(DeltaTime))
+		return -1;
+
+	return _int();
+}
+
+_int CCan::Late_Update(_double DeltaTime)
+{
+	if (nullptr == m_pRenderer)
+		return -1;
+
+	if (0 > __super::Late_Update(DeltaTime))
+		return -1;
+
+	return m_pRenderer->Add_RenderGroup(CRenderer::NONALPHA, this);
+}
+
+HRESULT CCan::Render()
+{
+	if (nullptr == m_pModel)
+		return E_FAIL;
+
+	if (FAILED(__super::Render()))
+		return E_FAIL;
+
+	if (FAILED(SetUp_ConstantTable()))
+		return E_FAIL;
+
+	_uint		iNumMaterials = (_uint)m_pModel->Get_NumMaterials();
+
+	m_pModel->Bind_Buffers();
+
+	for (_uint i = 0; i < iNumMaterials; ++i)
+	{
+		m_pModel->SetUp_TextureOnShader("g_DiffuseTexture", i, aiTextureType_DIFFUSE);
+		m_pModel->Render(i, 0);
+	}
+
+	return S_OK;
+}
+
+HRESULT CCan::SetUp_Components()
+{
+	/* For.Com_Renderer */
+	if (FAILED(__super::SetUp_Components(STATIC_SCENE, L"Component_Renderer", L"Com_Renderer", (CComponent**)&m_pRenderer)))
+		return E_FAIL;
+
+	/* For.Com_Model */
+	if (FAILED(__super::SetUp_Components(STATIC_SCENE, L"Component_Model_Can", L"Com_Model", (CComponent**)&m_pModel)))
+		return E_FAIL;
+
+	/* For.Com_Transform */
+
+	if (FAILED(__super::SetUp_Components(STATIC_SCENE, L"Component_Transform", L"Com_Transform", (CComponent**)&m_pTransform)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CCan::SetUp_ConstantTable()
+{
+	if (nullptr == m_pModel)
+		return E_FAIL;
+
+	CManagement*	pManagement = GET_INSTANCE(CManagement);
+
+	m_pModel->SetUp_ValueOnShader("g_WorldMatrix", &XMMatrixTranspose(m_pTransform->Get_WorldMatrix()), sizeof(_matrix));
+	m_pModel->SetUp_ValueOnShader("g_ViewMatrix", &XMMatrixTranspose(pManagement->Get_Transform(CPipeLine::D3DTS_VIEW)), sizeof(_matrix));
+	m_pModel->SetUp_ValueOnShader("g_ProjMatrix", &XMMatrixTranspose(pManagement->Get_Transform(CPipeLine::D3DTS_PROJ)), sizeof(_matrix));
+
+	RELEASE_INSTANCE(CManagement);
+
+	return S_OK;
+}
+
+CCan * CCan::Create(DEVICES)
+{
+	CCan*		pInstance = new CCan(pDevice, pDeviceContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX("Failed to Creating CStageMap");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CObj * CCan::Clone(void * pArg)
+{
+	CCan*		pInstance = new CCan(*this);
+
+	if (FAILED(pInstance->Initialize_Clone(pArg)))
+	{
+		MSG_BOX("Failed to Creating CStageMap");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CCan::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pCollider);
+	Safe_Release(m_pTransform);
+	Safe_Release(m_pModel);
+	Safe_Release(m_pRenderer);
+}
--- KatamariDamacy/Client/Codes/PlayerBall.cpp
+++ KatamariDamacy/Client/Codes/PlayerBall.cpp
@@ -15,44 +15,154 @@ CPlayerBall::CPlayerBall(const CPlayerBall & other)
 
 HRESULT CPlayerBall::Initialize_Prototype()
 {
-	return E_NOTIMPL;
+	if (FAILED(__super::Initialize_Prototype()))
+		return E_FAIL;
+
+	return S_OK;
 }
 
 HRESULT CPlayerBall::Initialize_Clone(void * pArg)
 {
-	return E_NOTIMPL;
+	if (FAILED(__super::Initialize_Clone(pArg)))
+		return E_FAIL;
+
+	if (FAILED(SetUp_Components()))
+		return E_FAIL;
+
+	CManagement*	pManagement = GET_INSTANCE(CManagement);
+
+	m_pPlayerTransform = dynamic_cast<CTransform*>(pManagement->GetComponent(STAGEONE_SCENE, TEXT("Layer_Player"), TEXT("Com_Transform")));
+
+	RELEASE_INSTANCE(CManagement);
+
+	return S_OK;
 }
 
 _int CPlayerBall::Update(_double DeltaTime)
 {
+	if (0 > __super::Update(DeltaTime))
+		return -1;
+
+	m_pCollider->Update_State(m_pTransform->Get_WorldMatrix());
+
 	return _int();
 }
 
 _int CPlayerBall::Late_Update(_double DeltaTime)
 {
-	return _int();
+	if (nullptr == m_pRenderer)
+		return -1;
+
+	if (0 > __super::Late_Update(DeltaTime))
+		return -1;
+
+	return m_pRenderer->Add_RenderGroup(CRenderer::NONALPHA, this);
 }
 
 HRESULT CPlayerBall::Render()
 {
-	return E_NOTIMPL;
+	if (nullptr == m_pModel)
+		return E_FAIL;
+
+	if (FAILED(__super::Render()))
+		return E_FAIL;
+
+	if (FAILED(SetUp_ConstantTable()))
+		return E_FAIL;
+
+	_uint		iNumMaterials = m_pModel->Get_NumMaterials();
+
+	m_pModel->Bind_Buffers();
+
+	for (_uint i = 0; i < iNumMaterials; ++i)
+	{
+		m_pModel->SetUp_TextureOnShader("g_DiffuseTexture", i, aiTextureType_DIFFUSE);
+		m_pModel->Render(i, 1);
+	}
+
+#ifdef _DEBUG
+	m_pCollider->Render();
+#endif
+
+	return S_OK;
 }
 
 HRESULT CPlayerBall::SetUp_Components()
 {
-	return E_NOTIMPL;
+	/* For.Com_Renderer */
+	if (FAILED(__super::SetUp_Components(STATIC_SCENE, TEXT("Component_Renderer"), TEXT("Com_Renderer"), (CComponent**)&m_pRenderer)))
+		return E_FAIL;
+
+	/* For.Com_Model */
+	if (FAILED(__super::SetUp_Components(STAGEONE_SCENE, TEXT("Component_Model_PlayerBall"), TEXT("Com_Model"), (CComponent**)&m_pModel)))
+		return E_FAIL;
+
+	/* For.Com_Transform */
+
+	CTransform::TRANSFORMDESC	TransformDesc;
+	TransformDesc.fSpeedPerSec = 3.f;
+
+	if (FAILED(__super::SetUp_Components(STATIC_SCENE, TEXT("Component_Transform"), TEXT("Com_Transform"), (CComponent**)&m_pTransform, &TransformDesc)))
+		return E_FAIL;
+
+	/* For.Com_Collider */
+	CCollider::COLLIDERDESC		ColliderDesc;
+	ColliderDesc.vSize = _float3(1.f, 1.f, 1.f);
+
+	if (FAILED(__super::SetUp_Components(STATIC_SCENE, TEXT("Component_Collider_AABB"), TEXT("Com_Collider"), (CComponent**)&m_pCollider, &ColliderDesc)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CPlayerBall::SetUp_ConstantTable()
+{
+	if (nullptr == m_pModel)
+		return E_FAIL;
+
+	CManagement*	pManagement = GET_INSTANCE(CManagement);
+
+	m_pModel->SetUp_ValueOnShader("g_WorldMatrix", &XMMatrixTranspose(m_pTransform->Get_WorldMatrix()), sizeof(_matrix));
+	m_pModel->SetUp_ValueOnShader("g_ViewMatrix", &XMMatrixTranspose(pManagement->Get_Transform(CPipeLine::D3DTS_VIEW)), sizeof(_matrix));
+	m_pModel->SetUp_ValueOnShader("g_ProjMatrix", &XMMatrixTranspose(pManagement->Get_Transform(CPipeLine::D3DTS_PROJ)), sizeof(_matrix));
+
+	RELEASE_INSTANCE(CManagement);
+
+	return S_OK;
 }
 
 CPlayerBall * CPlayerBall::Create(DEVICES)
 {
-	return nullptr;
+	CPlayerBall*		pInstance = new CPlayerBall(pDevice, pDeviceContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX("Failed to Creating CPlayerBall");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
 }
 
 CObj * CPlayerBall::Clone(void * pArg)
 {
-	return nullptr;
+	CPlayerBall*		pInstance = new CPlayerBall(*this);
+
+	if (FAILED(pInstance->Initialize_Clone(pArg)))
+	{
+		MSG_BOX("Failed to Creating CPlayerBall");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
 }
 
 void CPlayerBall::Free()
 {
+	__super::Free();
+
+	Safe_Release(m_pCollider);
+	Safe_Release(m_pTransform);
+	Safe_Release(m_pModel);
+	Safe_Release(m_pRenderer);
 }
